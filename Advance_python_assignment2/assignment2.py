d32e14913645f1fac30b8c7d6efa46e8b2bbbf12
PreritSaxena <preritsaxena2103@gmail.com>
2024-07-22 12:38:12 +0530

    arranged files of assignment

--- Advance_python_assignment2/assignment2.py
+++ Advance_python_assignment2/assignment2.py
@@ -2,24 +2,24 @@
 
 # 1. Write a function in python to read the content from a text file "ABC.txt" line by line and display the same on screen.
 
-# with open('ABC.txt', 'r') as file:
-#     for line in file:
-#         print(line, end='') 
+with open('ABC.txt', 'r') as file:
+    for line in file:
+        print(line, end='') 
 
 #2 Write a function in Python to count and display the total number of words in a text file "ABC.txt"
-# def count_words_in_file(filename="ABC.txt"):
-#     total_words = 0
-#     try:
-#         with open(filename, 'r') as file:
-#             for line in file:
-#                 words = line.split()
-#                 total_words += len(words)
-#         print(f"Total number of words: {total_words}")
-#     except FileNotFoundError:
-#         print(f"The file {filename} does not exist.")
+def count_words_in_file(filename="ABC.txt"):
+    total_words = 0
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                words = line.split()
+                total_words += len(words)
+        print(f"Total number of words: {total_words}")
+    except FileNotFoundError:
+        print(f"The file {filename} does not exist.")
 
-# Call the function
-# count_words_in_file()
+
+count_words_in_file()
 
 # 3. Write a function in Python to count uppercase character in a text file "ABC.txt"
 
@@ -35,7 +35,7 @@ def count_uppercase_characters(filename="ABC.txt"):
     except FileNotFoundError:
         print(f"The file {filename} does not exist.")
 
-# Call the function
+
 count_uppercase_characters()
 
 # 4
@@ -50,6 +50,9 @@ def display_words(filename="ABC.txt"):
     except FileNotFoundError:
         print(f"The file {filename} does not exist.")
 
-# Call the function
+
 display_words()
+
+# 5
+# occurence of word INDIA IN FILE anudip.txt
 
